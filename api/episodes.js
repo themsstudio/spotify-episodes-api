29de9aafb0de6f3a38dd00eb4b74e93ba03b0f2b
themsstudio <ms@ms.studio>
2025-05-20 17:40:54 +0400

    Update episodes.js

--- api/episodes.js
+++ api/episodes.js
@@ -1,3 +1,5 @@
+import { Buffer } from 'buffer';
+
 export default async function handler(req, res) {
   const client_id = process.env.SPOTIFY_CLIENT_ID;
   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
@@ -13,7 +15,7 @@ export default async function handler(req, res) {
 
   const { access_token } = await authResponse.json();
 
-  const showId = "0u41erQGzWWaE6xjmZMDNN"; // Your Spotify Show ID
+  const showId = "0u41erQGzWWaE6xjmZMDNN";
   const episodesResponse = await fetch(`https://api.spotify.com/v1/shows/${showId}/episodes?market=US&limit=5`, {
     headers: {
       Authorization: `Bearer ${access_token}`,
